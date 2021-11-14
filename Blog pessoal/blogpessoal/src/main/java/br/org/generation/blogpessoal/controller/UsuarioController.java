93fff6017f2d70445432cb34f3ae616aedfcc4d8
Dejsilva <dejs_silva@hotmail.com>
2021-11-14 13:32:29 -0300

    security loja de games

--- Blog pessoal/blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
+++ Blog pessoal/blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/UsuarioController.java
@@ -10,14 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import br.org.generation.blogpessoal.model.Postagem;
 import br.org.generation.blogpessoal.model.Usuario;
 import br.org.generation.blogpessoal.model.UsuarioLogin;
 import br.org.generation.blogpessoal.repository.UsuarioRepository;
@@ -87,17 +84,10 @@ public class UsuarioController {
 	 * atualizados são exibidos.
 	 */
 	@PutMapping("/atualizar")
-	public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario) {
+	public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario ) {
 		return usuarioService.atualizarUsuario(usuario)
 			.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
 			.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 	}
 	
-	@GetMapping("/{id}")
-	public ResponseEntity<Usuario> getById(@PathVariable long id){
-		return usuarioRepository.findById(id)
-				.map(resposta -> ResponseEntity.ok(resposta))
-				.orElse(ResponseEntity.notFound().build());
-
-}
 }
