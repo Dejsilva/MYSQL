c647df27d8894fa7b7986acee1623faf8fd7882f
Dejsilva <dejs_silva@hotmail.com>
2021-11-16 15:09:51 -0300

    teste junit

---
+++ Blog pessoal/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,80 @@
+package br.org.generation.blogpessoal.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.org.generation.blogpessoal.model.Postagem;
+import br.org.generation.blogpessoal.repository.PostagemRepository;
+
+//@RestController é responsavel por reponder as http enviadas para um endereço
+//@RequestMapping indica o endereço que a controller vai responder as requisições
+//@CrossOrigin indica que a classe controladora permitira o recebimento de requisições fora do dominio (local host)
+/* Injeção de Dependência (@Autowired): Consiste  na  maneira,  ou  seja,  na  implementação 
+* utilizada pelo  Spring  Framework  de  aplicar  a  Inversão  de  Controle  quando  for 
+* necessário*/
+
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*" , allowedHeaders = "*")
+public class PostagemController {
+	
+	
+	@Autowired
+	private PostagemRepository postagemRepository;
+
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> getAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+		
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable long id){
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+}
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> postPostagem(@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+		
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> putPostagem(@RequestBody Postagem postagem){
+		return postagemRepository.findById(postagem.getId())
+		.map(resposta ->  ResponseEntity.ok(postagemRepository.save(postagem)))
+		.orElse(ResponseEntity.notFound().build());	
+		
+	
+	}
+	@DeleteMapping({"/{id}"})
+	public ResponseEntity <?> delete(@PathVariable long id) {
+		   return postagemRepository.findById(id)
+		           .map(checagem -> {
+		        	 postagemRepository.deleteById(id);
+		             return ResponseEntity.ok().build();
+		           	}).orElse(ResponseEntity.notFound().build());
+
+	   			   			
+}
+	}
